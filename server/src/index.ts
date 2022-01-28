9f6d49d3e63d14832826b080dd3ba7bc6f4cf23b
ocnelavoc <ocnelavoc@gmail.com>
2022-01-28 09:57:50 +0200

    [refactor]patch

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,6 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
-const UserController = require("./controllers/users-controller");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
