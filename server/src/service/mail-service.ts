08d7d207722db9c73456bbdb3f5d17d9a8bcd8dc
ocnelavoc <ocnelavoc@gmail.com>
2022-02-02 07:28:52 +0200

    [fix]

--- server/src/service/mail-service.ts
+++ server/src/service/mail-service.ts
@@ -1,19 +1,23 @@
-// @ts-ignore
+
 import nodemailer from "nodemailer";
 
 export default class MailService {
   transporter;
   constructor() {
-    console.log(process.env.SMTP_HOST);
-
     this.transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
-      secure: false,
+      service: "Gmail",
       auth: {
         user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD,
-      },
+        pass: process.env.SMTP_PASSWORD
+      }
+
+      // host: process.env.SMTP_HOST,
+      // port: process.env.SMTP_PORT,
+      // secure: false,
+      // auth: {
+      //   user: process.env.SMTP_USER,
+      //   pass: process.env.SMTP_PASSWORD,
+      // },
     });
   }
   async sendActivationMail(to: string, link: string) {
