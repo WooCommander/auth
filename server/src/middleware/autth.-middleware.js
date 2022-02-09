a61a0303205fdc091fe257301f0b30a6ec04f536
ocnelavoc <ocnelavoc@gmail.com>
2022-02-09 07:24:43 +0200

    [fix]

--- server/src/middleware/autth.-middleware.js
+++ server/src/middleware/autth.-middleware.js
@@ -1,6 +1,21 @@
+const ApiError = require("../exceptions/api-error")
+const tokenService = require('../service/token-service')
 module.exports = function (req, res, next) {
   try {
-
+    const authorizatonHeader = req.headers.authorizaton;
+    if (!authorizatonHeader) {
+      return next(ApiError.UnauthorizedError());
+    }
+    const accessToken = authorizatonHeader.split(" ")[1];
+    if (!accessToken) {
+      return next(ApiError.UnauthorizedError());
+    }
+    const userData = tokenService.validateAccessToken(accessToken)
+    if (!userData) {
+      return next(ApiError.UnauthorizedError());;
+    }
+    req.user = userData;
+    next();
   } catch (error) {
     return next(ApiError.UnauthorizedError());
   }
--- server/src/router/index.ts
+++ server/src/router/index.ts
@@ -3,10 +3,10 @@ import userController from "../controllers/users-controller";
 import { body } from "express-validator";
 
 
-// import authMiddleware from "../middlewares/auth-middleware";
+import authMiddleware from "../middlewares/auth-middleware";
 
 const authRouter = Router();
-
+// const authMiddleware = require("../middleware/auth-middleware")
 authRouter.post(
   "/registration",
   body("email").isEmail(),
@@ -20,7 +20,7 @@ authRouter.post("/login", userController.login);
 authRouter.post("/logout", userController.logout);
 authRouter.get("/activate/:link", userController.activate);
 authRouter.get("/refresh", userController.refresh);
-authRouter.get("/users", userController.getUsers);
+authRouter.get("/users", authMiddleware, userController.getUsers);
 authRouter.post("/registration", body('email').isEmpty, body('password').isLength({ min: 3, max: 32 }), userController.registration);
 // authRouter.get("/registration", userController.registration);
 
